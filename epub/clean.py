f571ee406d83bdbf31c5183384b71b69a191f64c
finetuneanon <82650881+finetuneanon@users.noreply.github.com>
2021-05-03 20:52:01 +0200

    Create clean.py

---
+++ epub/clean.py
@@ -0,0 +1,44 @@
+import sys
+import ftfy
+import os
+import argparse
+from pathlib import Path
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--source-folder", help="source folder with epubs", type=str, required=True)
+parser.add_argument("-o", "--output-folder", help="output folder with txts", type=str, required=True)
+args = parser.parse_args()
+
+source = Path(args.source_folder)
+output = Path(args.output_folder)
+
+if not os.path.isdir(source):
+    print("source has to be a folder")
+    sys.exit(1)
+
+if not os.path.isdir(output):
+    print("output has to be a folder")
+    sys.exit(1)
+
+print("Source: " + args.source_folder + "\nOutput: " + args.output_folder)
+
+for root, subdirs, files in os.walk(args.source_folder):
+    for file in files:
+        file = str(Path(root) / Path(file))
+        if not file.lower().endswith('txt'):
+            print("skip: " + repr(file))
+            continue
+        out = str(output / Path(file).stem)
+        postfix = ".txt"
+        i = 0
+        while os.path.isfile(out + postfix):
+            i += 1
+            postfix = "-" + str(i) + ".txt"
+        out += postfix
+        print(repr(file) + " to " + repr(out))
+        with open(file, 'r', encoding='utf-8') as fh:
+            text = fh.read()
+        text = ftfy.fix_text(text).replace(' …', '...').replace('…', '...').replace('\n\n\n', '\n').replace('\n\n', '\n')
+        text += "<|endoftext|>"
+        with open(out, 'w', encoding='utf-8') as fh:
+            fh.write(text)
