85e3026fbf389d7080b72555eacc976c5990975a
finetuneanon <82650881+finetuneanon@users.noreply.github.com>
2021-05-14 15:30:30 +0200

    Update clean.py

--- epub/clean.py
+++ epub/clean.py
@@ -1,3 +1,6 @@
+# recommended for gpt-neo tfrecords: clean.py -u -r -s -i input/ -o clean/
+import random
+import re
 import sys
 import ftfy
 import os
@@ -5,42 +8,171 @@ import argparse
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--source-folder", help="source folder with epubs", type=str, required=True)
+parser.add_argument("-i", "--input-folder", help="input folder with epubs", type=str, required=True)
 parser.add_argument("-o", "--output-folder", help="output folder with txts", type=str, required=True)
+parser.add_argument("-u", "--unmarkdown", help="remove markdown formatting if detected", action='store_true')
+parser.add_argument("-r", "--remove-eot", help="remove final <|endoftext|> in file", action='store_true')
+parser.add_argument("-a", "--add-eot", help="add final <|endoftext|> to files", action='store_true')
+parser.add_argument("-s", "--split-eot", help="split into multiple files at <|endoftext|>", action='store_true')
+parser.add_argument("-x", "--shuffle", help="shuffle input file order and give numbered names", action='store_true')
+parser.add_argument("-c", "--collapse-newlines", help="collapse multiple newlines into one", action='store_true')
 args = parser.parse_args()
 
-source = Path(args.source_folder)
+source = Path(args.input_folder)
 output = Path(args.output_folder)
 
 if not os.path.isdir(source):
-    print("source has to be a folder")
+    print("input has to be a folder")
     sys.exit(1)
 
 if not os.path.isdir(output):
     print("output has to be a folder")
     sys.exit(1)
 
-print("Source: " + args.source_folder + "\nOutput: " + args.output_folder)
+print("Input: " + args.input_folder + "\nOutput: " + args.output_folder)
 
-for root, subdirs, files in os.walk(args.source_folder):
+from markdown import Markdown
+from io import StringIO
+
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
+
+# patching Markdown
+Markdown.output_formats["plain"] = unmark_element
+__md = Markdown(output_format="plain")
+__md.stripTopLevelTags = False
+
+
+def unmark(text):
+    return __md.convert(text)
+
+def space_fix(s):
+    have_open = False
+    need_space = False
+    need_now = False
+    was_space = False
+    no_space = False
+    r = ""
+    for i in range(len(s)):
+        if s[i] == '"':
+            if have_open:
+                need_space = True
+                need_now = True
+                have_open = False
+                if was_space:
+                    r = r[:-1]
+            else:
+                if need_space:
+                    r += " "
+                have_open = True
+                no_space = True
+            r += s[i]
+            was_space = False
+            continue
+        if need_space and need_now:
+            if not s[i].isspace():
+                r += " "
+            need_space = False
+            need_now = False
+        if s[i].isspace():
+            if no_space:
+                continue
+            need_space = False
+            was_space = True
+        else:
+            need_space = True
+            was_space = False
+        no_space = False
+        r += s[i]
+    return r
+
+punctuation = ['.', '?', '!', ',', ':', ';', '-', ']', ')', '}', '[', '(', '{']
+def punctuation_fix(s):
+    r = ""
+    was_punctuation = False
+    was_space = True
+    was_no_post = False
+    needs_pre = False
+    was_quote = False
+    for i in range(len(s)):
+        if s[i] in punctuation:
+            needs_pre = s[i] in ['[', '(', '{']
+            if not needs_pre and not was_quote and was_space:
+                if i > 0 and r[-1] != '\n':
+                    r = r[:-1]
+            was_punctuation = True
+            was_no_post = s[i] in ['-', '[', '(', '{']
+        else:
+            if was_punctuation and not was_no_post and s[i] not in ['"', "'"] and not s[i].isspace():
+                r += " "
+            was_punctuation = False
+            was_no_post = False
+        was_space = s[i].isspace()
+        if not was_space:
+            was_quote = s[i] == '"'
+        r += s[i]
+    return r
+
+all_files = []
+
+for root, subdirs, files in os.walk(args.input_folder):
     for file in files:
         file = str(Path(root) / Path(file))
         if not file.lower().endswith('txt'):
             print("skip: " + repr(file))
             continue
-        out = str(output / Path(file).stem)
+        all_files.append(file)
+
+file_id = 0
+if args.shuffle:
+    random.shuffle(all_files)
+
+for file in all_files:
+    out = str(output / Path(file).stem)
+    if args.shuffle:
+        out = str(output / Path(f"clean-{file_id:05d}"))
+        file_id += 1
+    with open(file, 'r', encoding='utf-8') as fh:
+        text = fh.read()
+    text = re.sub(r'( |\t)+', ' ', text)
+    text = re.sub(r'(^|\n)( |\t)+', r'\1', text)
+    text = ftfy.fix_text(text).replace(' …', '...').replace('…', '...').replace("»", "\"").replace("«", "\"").replace('\N{SOFT HYPHEN}', '').replace('\u200b', '')
+    text = text.replace('\r\n', '\n').replace('\r', '\n')
+    if args.collapse_newlines:
+        text = text.replace('\n\n\n', '\n').replace('\n\n', '\n')
+    lines = text.split("\n")
+    for i in range(len(lines)):
+        lines[i] = punctuation_fix(space_fix(lines[i].strip()))
+    text = "\n".join(lines)
+    text = re.sub(r'https?:\/\/[^\s\)\]\}]*', '(Link removed)', text)
+    text = re.sub(r"\s*$", "", text)
+    if args.unmarkdown and re.search(r'(^|\n)#', text) and re.search(r'_.*_', text):
+        text = unmark(text)
+    if args.add_eot:
+        text = text + "<|endoftext|>"
+    text = re.sub(r"(\s*<\|endoftext\|>\s*)+", "<|endoftext|>", text)
+    if args.remove_eot:
+        text = re.sub(r"<\|endoftext\|>$", "", x)
+    if args.split_eot:
+        text = text.split("<|endoftext|>")
+    else:
+        text = [text]
+    for out_text in text:
+        outname = out
         postfix = ".txt"
         i = 0
-        while os.path.isfile(out + postfix):
+        while os.path.isfile(outname + postfix):
             i += 1
-            postfix = "-" + str(i) + ".txt"
-        out += postfix
-        print(repr(file) + " to " + repr(out))
-        with open(file, 'r', encoding='utf-8') as fh:
-            text = fh.read()
-        text = ftfy.fix_text(text).replace(' …', '...').replace('…', '...').replace("»", "\"").replace("«", "\"").replace('\r\n', '\n').replace('\r', '\n').replace('\n\n\n', '\n').replace('\n\n', '\n').replace('\N{SOFT HYPHEN}', '').replace('\u200b', '')
-        text = "\n".join(map(lambda x: x.strip(), text.split('\n')))
-        text += "<|endoftext|>"
-        text = text.replace("<|endoftext|>\n<|endoftext|>", "<|endoftext|>").replace("<|endoftext|><|endoftext|>", "<|endoftext|>").replace("<|endoftext|><|endoftext|>", "<|endoftext|>").replace("\n<|endoftext|>", "<|endoftext|>").replace("\n<|endoftext|>", "<|endoftext|>").replace("<|endoftext|>\n", "<|endoftext|>").replace("<|endoftext|>\n", "<|endoftext|>")
-        with open(out, 'w', encoding='utf-8') as fh:
-            fh.write(text)
+            postfix = f"-{i:05d}.txt"
+        outname += postfix
+        print(repr(file) + " to " + repr(outname))
+        with open(outname, 'w', encoding='utf-8') as fh:
+            fh.write(out_text)
