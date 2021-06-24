ec95ca2f1ec52870f0d9b37c518b90ccd2912bf6
finetuneanon <82650881+finetuneanon@users.noreply.github.com>
2021-06-24 13:29:04 +0200

    Update encode.py

--- encode.py
+++ encode.py
@@ -1,52 +1,70 @@
+import os
+import random
 from secrets import randbelow
 import numpy as np
 import json
 import sys
+
+import argparse
+from pathlib import Path
+
+from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
+import functools
 from transformers import AutoTokenizer
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--source-folder", help="source folder with epubs", type=str, required=True)
+parser.add_argument("-o", "--output", help="output numpy memmap", type=str, required=True)
+args = parser.parse_args()
+
+source = Path(args.source_folder)
+output = Path(args.output)
+
+data = []
+
+print("listing")
+for root, subdirs, files in os.walk(args.source_folder):
+    for file in files:
+        file = str(Path(root) / Path(file))
+        if not file.lower().endswith('txt'):
+            continue
+        data.append(file)
+
+random.shuffle(data)
+
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 text = ""
 chunks = []
 token_buf = []
-max_chars = 5000000
-for line in sys.stdin:
-    text += line
-    if len(text) >= max_chars:
-        text = text.replace('<|endoftext|>\n', '<|endoftext|>')
-        text = text.replace('<|endoftext|>\n', '<|endoftext|>')
-        tokens = tokenizer.encode(text[0:max_chars], truncation=False)[8:-8]
+
+def tokenize_file(tokenize, filename):
+    with open(filename, "rb") as fh:
+        text = fh.read().decode('utf-8', 'surrogateescape')
+        if len(text) < 1:
+            return
+        text = text + '<|endoftext|>'
+        tokens = tokenizer.encode(text, truncation=False)
         if tokens is None or len(tokens) < 1:
-            continue
-        text = text[max_chars:]
-        token_buf = tokens
-        while len(token_buf) > 2048:
-            chunks.append(np.array(token_buf[0:2048], dtype=np.uint16))
-            token_buf = token_buf[2048:]
-while len(text) > 0:
-    tokens = tokenizer.encode(text)
-    tokens = tokens[8:-8]
-    token_buf = tokens
-    text = ""
-    while len(token_buf) > 2048:
-        chunks.append(np.array(token_buf[0:2048], dtype=np.uint16))
-        token_buf = token_buf[2048:]
-chunks = np.array(chunks, dtype=np.uint16)
+            return
+        return np.array(tokens, dtype=np.uint16)
+
+print("tokenize", flush=True)
+token_buf = process_map(functools.partial(tokenize_file, tokenizer), data, chunksize=20, max_workers=8)
+token_buf = np.concatenate(token_buf)
+
 print("save", flush=True)
-print(chunks.shape, flush=True)
-indexes = list(range(chunks.shape[0]))
+n_samples = len(token_buf) // 2048
+print("#samples", n_samples)
+mmap = np.memmap(output, mode="w+", dtype="uint16", shape=(n_samples, 2048))
 
-def shuffle(x):
-    n = len(x)
-    for i in range(n-1):
-        j = i + randbelow(n - i)
-        t = x[i]
-        x[i] = x[j]
-        x[j] = t
-        yield x[i]
-    yield x[n-1]
+indexes = list(range(n_samples))
+random.shuffle(indexes)
 
-mmap = np.memmap("fb-2048.map", mode="w+", dtype="uint16", shape=chunks.shape)
-for target, source in enumerate(shuffle(indexes)):
-    mmap[target, :] = chunks[source, :]
+for target, source in tqdm(enumerate(indexes)):
+    mmap[target, :] = np.array(token_buf[source * 2048:(source + 1) * 2048], dtype=np.uint16)
+
 mmap.flush()
-print("\nDone", flush=True)
+
+print("done", flush=True)
